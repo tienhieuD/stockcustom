85dddb324efa1352c819207225b268e87749940f
duongtienhieu <duongnguyen1216@gmail.com>
2018-06-19 23:15:08 +0700

    formated code

--- models/models.py
+++ models/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-
-from odoo import models, api
+from odoo import models, api, fields
+from odoo.exceptions import ValidationError
+from odoo.tools.translate import _
 
 
 class ReturnPicking(models.TransientModel):
@@ -13,8 +14,18 @@ class ReturnPicking(models.TransientModel):
             for line in res.get('product_return_moves'):
                 line[2]['to_refund'] = True
         return res
+
+
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
-    _sql_constraints = {
-        ('client_order_ref_uniq', 'unique(client_order_ref)', 'Customer Reference không được trùng')
-    }
+
+    _sql_constraints = [('client_order_ref_uniq', 'unique(client_order_ref)',
+                         u'Tham chiếu khách hàng (Customer Reference) không được trùng nhau.')]
+
+    @api.constrains('client_order_ref')
+    def _check_client_order_ref(self):
+        customer_reference = self.sudo().search([]).mapped('client_order_ref')
+        for rec in self:
+            if rec.client_order_ref in customer_reference:
+                raise ValidationError(_('Tham chiếu khách hàng (Customer Reference) không được trùng nhau.'))
+
